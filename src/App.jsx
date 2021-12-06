08ff120c2f99dc6cba0a306c8d20169ca325cf29
educiter <you@example.com>
2021-12-06 12:50:33 +0300

    сетевой запрос

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 
-import styled from "styled-components";
-
 function App() {
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const [data, setData] = React.useState(null);
+
+  const api = "https://jsonplaceholder.typicode.com/todos/1";
+
+  React.useEffect(() => {
+    if (!data) {
+      setLoading(true);
+      fetch(api)
+        .then((response) => {
+          if (response.ok && response.status === 200) {
+            return response.json();
+          } else {
+            setError(response.status);
+            setLoading(false);
+          }
+        })
+        .then((json) => {
+          setData(json);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(err);
+          setLoading(false);
+        });
+    }
+  }, [data]);
+
   return (
     <div>
-      <Block bgcolor="green">ТЕКСТ</Block>
-      <A>текст 2</A>
+      {loading && <div>loading...</div>}
+      {data && !loading && <div>{data.title}</div>}
+      {error && !loading && <div>error!</div>}
     </div>
   );
 }
-
-const Block = styled.div`
-  background-color: ${(props) => props.bgcolor || "red"};
-  font-size: 20px;
-  height: 300px;
-  width: 100%;
-  color: #fff;
-`;
-
-const A = styled(Block)`
-  font-size: 40px;
-`;
 
 export default App;
